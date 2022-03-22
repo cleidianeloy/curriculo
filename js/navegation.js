1f9906e5bcaeb31a509a90eb78ddb5eb9f09e38e
cleidianeloy <cleidianeloy@hotmail.com>
2022-03-22 10:27:35 -0300

    tentando melhorar carregamento - testando

--- js/navegation.js
+++ js/navegation.js
@@ -5,23 +5,25 @@ var sectionsPositions = $.map(realSections, function (section){
     return $(section).position().top;
 });
 
-$(window).scroll(function(){
-    // debugger;
-    var userPosition = $(this).scrollTop();
-    for(var i = 0; i < sectionsPositions.length; i++){
-        var height = $(realSections[i]).height() ;
+// $(window).scroll(function(){
+//     // debugger;
+//     var userPosition = $(this).scrollTop();
+//     for(var i = 0; i < sectionsPositions.length; i++){
+//         var height = $(realSections[i]).height() ;
     
-        if(userPosition <= sectionsPositions[i] + height - (height * 0,50) && userPosition >= sectionsPositions[i] ){
-            $(links[i - 1]).attr('class', 'on');
-           $(links[i - 1]).siblings('a').removeClass('on');
-        }if(userPosition <= sectionsPositions[0] + height - (height * 0,50)){
-            $(links).removeClass('on');
-        }
-    }
-}) 
-function carregou(){
+//         if(userPosition <= sectionsPositions[i] + height - (height * 0,50) && userPosition >= sectionsPositions[i] ){
+//             $(links[i - 1]).attr('class', 'on');
+//            $(links[i - 1]).siblings('a').removeClass('on');
+//         }if(userPosition <= sectionsPositions[0] + height - (height * 0,50)){
+//             $(links).removeClass('on');
+//         }
+//     }
+// }) 
+async function carregou(){
     setTimeout(()=>{
         $("#loading").hide();
+        $('menu').show();
+        $('main').show();
     },4000)
 }
 
