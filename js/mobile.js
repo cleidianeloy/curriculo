d0f8a7668d5e3a426ef100389262b95a48cf53ea
cleidianeloy <cleidianeloy@hotmail.com>
2022-09-05 18:49:21 -0300

    algumas mudanças sobre acessibilidade - retornei o código para aparecer em que pagina está

--- js/mobile.js
+++ js/mobile.js
@@ -1,4 +1,4 @@
-$('menu').append('<svg id="hamburguer" aria-expanded="false" role="button" aria-label="menu" xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 0 24 24" width="48" fill="#FFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>');
+$('nav').append('<svg id="hamburguer" aria-expanded="false" role="button" aria-label="menu" xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 0 24 24" width="48" fill="#FFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>');
 var isMobile =  () => {
     var isMobile = false;
     var width = $(window).width();
@@ -10,20 +10,20 @@ var isMobile =  () => {
     return isMobile;
 };
 var menuMobile = () =>{
-    $('menu a').hide();
-    $('menu #hamburguer').show();
+    $('nav a').hide();
+    $('nav #hamburguer').show();
     $('#hamburguer').click(function(){
         var ariaExpanded = $(this).attr('aria-expanded');
         if(ariaExpanded == 'false'){
-            $('menu').attr('class','ativo');
-            $('menu a').show();
+            $('nav').attr('class','ativo');
+            $('nav a').show();
             $(this).attr('aria-expanded', 'true');
         }else{
-            $('menu a').hide();
-            $('menu').removeClass('ativo');
-            $('menu a').click(()=>{
-                $('menu').removeClass('ativo');
-                $('menu a').hide();
+            $('nav a').hide();
+            $('nav').removeClass('ativo');
+            $('nav a').click(()=>{
+                $('nav').removeClass('ativo');
+                $('nav a').hide();
                 $(this).attr('aria-expanded', 'false');
             })
             $(this).attr('aria-expanded', 'false');
@@ -33,8 +33,8 @@ var menuMobile = () =>{
 
 }
 var menuDesktop = () =>{
-    $('menu a').show();
-    $('menu #hamburguer').hide();
+    $('nav a').show();
+    $('nav #hamburguer').hide();
 }
 var vertical = () =>{
     $('.horizontal').hide();
--- js/navegation.js
+++ js/navegation.js
@@ -1,28 +1,30 @@
 var sections = $('section');
-var links = $.makeArray($('menu a'));
-var realSections = $.makeArray(sections);
-var sectionsPositions = $.map(realSections, function (section){
-    return $(section).position().top;
-});
+var links = $.makeArray($('nav a'));
 
-// $(window).scroll(function(){
-//     // debugger;
-//     var userPosition = $(this).scrollTop();
-//     for(var i = 0; i < sectionsPositions.length; i++){
-//         var height = $(realSections[i]).height() ;
-    
-//         if(userPosition <= sectionsPositions[i] + height - (height * 0,50) && userPosition >= sectionsPositions[i] ){
-//             $(links[i - 1]).attr('class', 'on');
-//            $(links[i - 1]).siblings('a').removeClass('on');
-//         }if(userPosition <= sectionsPositions[0] + height - (height * 0,50)){
-//             $(links).removeClass('on');
-//         }
-//     }
-// }) 
+
+$(window).scroll(function(){
+    // debugger;
+    var realSections = $.makeArray(sections);
+    var sectionsPositions = $.map(realSections, function (section){
+        return $(section).position().top;
+    });
+    var userPosition = $(this).scrollTop();
+    for(var i = 0; i <= sectionsPositions.length; i++){
+        var height = $(realSections[i]).height();
+        if(userPosition <= sectionsPositions[0] + height - (height * 0.08)){
+            $(links).removeClass('on');
+        }else 
+        if(userPosition <= (sectionsPositions[i] + height - (height * 0.08)) && userPosition >= sectionsPositions[i] ){
+            $(links[i - 1]).attr('class', 'on');
+           $(links[i - 1]).siblings('a').removeClass('on');
+        }
+        
+    }
+}) 
 async function carregou(){
     setTimeout(()=>{
         $("#loading").hide();
-        $('menu').show();
+        $('nav').show();
         $('main').show();
     },4000)
 }
